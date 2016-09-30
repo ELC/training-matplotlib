52f95cd509d57bdb31b8053ae9f488fd801e5f3c
elcg <elcg@gmx.com>
2016-09-30 15:34:12 -0300

    Move files to correct folders

--- Freelance/SU-MA-DO/presentation.py
+++ Freelance/SU-MA-DO/presentation.py
@@ -1,5 +1,4 @@
 import mainfunction
-<<<<<<< HEAD:Freelance/SU-MA-DO/presentation.py
 from adjacency_lists import get_adj_list
 import timeit
 
@@ -105,40 +104,4 @@ def testing():
 
 if __name__ == "__main__":
     testing()
-=======
-import unittest
-"""
 
-Testing
-
-"""
-
-class TestMainFunction(unittest.TestCase):
-    
-    def test_get_pairs(self):
-        i = [1,2,3,4]
-        o = {
-            1:[2,3,4]
-            ,2:[3,4]
-            ,3:[4]
-            }
-        result = mainfunction.get_pairs(i)
-        self.assertEqual(result,o)
-    
-    def test_clean_vertex(self):
-        adj = {
-            1 : [2,3,4]
-            ,2 : [1,3,4]
-            ,3 : [1,2,4]
-            ,4 : [1,2,3]
-            }
-        vertex = 4
-        o = {
-            1 : [2,3]
-            ,2 : [1,3]
-            ,3 : [1,2]
-            ,4 : [1,2,3]
-            }
-        mainfunction.clean_vertex(adj,vertex)
-        self.assertEqual(adj ,o)
->>>>>>> origin/master:SU-MA-DO/test_unittest.py

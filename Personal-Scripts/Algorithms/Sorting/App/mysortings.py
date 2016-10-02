38d7ab76f632d978d565f3b5815b68692333919a
Ezequiel Leonardo Castaño <elcg@gmx.com>
2016-10-02 16:55:54 -0300

    Remove Duplicates

--- Personal-Scripts/Algorithms/Sorting/App/mysortings.py
+++ Personal-Scripts/Algorithms/Sorting/App/mysortings.py
@@ -81,21 +81,6 @@ def cocktailbubble(iterable):
     return iterable
 
 
-def gnome(iterable):
-    done = False
-    while not done:
-        done = True
-        for i in range(len(iterable) - 1):
-            if not iterable[i] < iterable[i + 1]:
-                done = False
-                j = i
-                while not (iterable[j] < iterable[j + 1]) and j >= 0:
-                    iterable[j], iterable[j + 1] = \
-                        iterable[j + 1], iterable[j]
-                    j -= 1
-    return iterable
-
-
 def insertion(iterable):
     done = False
     while not done:

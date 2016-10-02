19a4041cf6925f58f6b5782d16495d76ae5a2666
Ezequiel Leonardo Castaño <elcg@gmx.com>
2016-10-02 16:42:26 -0300

    Fix function signature

--- Katas-Challanges/Spoj/sj_2.py
+++ Katas-Challanges/Spoj/sj_2.py
@@ -15,7 +15,7 @@ for i in range(int(1)):
     h = []
     start_time = timeit.default_timer()
     for j in range(int(p[0])+1, int(p[1])-1):
-        if isprime(j, len(str(j))):
+        if isprime(j):
             h.append(j)
     print(len(h), "-----", h)
     print(timeit.default_timer() - start_time)

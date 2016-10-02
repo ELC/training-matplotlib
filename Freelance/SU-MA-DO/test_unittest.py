52b1cc36fb112eef39918737d6380ebec39da3d3
Ezequiel Leonardo Castaño <elcg@gmx.com>
2016-10-02 16:10:39 -0300

    Rename variables

--- Freelance/SU-MA-DO/test_unittest.py
+++ Freelance/SU-MA-DO/test_unittest.py
@@ -5,14 +5,14 @@ import unittest
 class TestMainFunction(unittest.TestCase):
 
     def test_get_pairs(self):
-        i = [1, 2, 3, 4]
-        o = {
+        input_ = [1, 2, 3, 4]
+        output = {
             1: [2, 3, 4],
             2: [3, 4],
             3: [4]
             }
-        result = mainfunction.get_pairs(i)
-        self.assertEqual(result, o)
+        result = mainfunction.get_pairs(input_)
+        self.assertEqual(result, output)
 
     def test_clean_vertex(self):
         adj = {
@@ -22,11 +22,11 @@ class TestMainFunction(unittest.TestCase):
             4: [1, 2, 3]
             }
         vertex = 4
-        o = {
+        output = {
             1: [2, 3],
             2: [1, 3],
             3: [1, 2],
             4: [1, 2, 3]
             }
         mainfunction.clean_vertex(adj, vertex)
-        self.assertEqual(adj, o)
+        self.assertEqual(adj, output)

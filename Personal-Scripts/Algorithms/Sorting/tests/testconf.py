8057331c0772a2edfbeff1010be904c164c8918e
Ezequiel Leonardo Castaño <elcg@gmx.com>
2016-10-02 15:59:09 -0300

    Use enumerate builtin

--- Personal-Scripts/Algorithms/Sorting/tests/testconf.py
+++ Personal-Scripts/Algorithms/Sorting/tests/testconf.py
@@ -4,8 +4,8 @@ n = 500
 a = [i for i in range(n)]
 LENGTH = 5
 c = [a[:] for _ in range(LENGTH)]
-for i in range(len(c)):
-    shuffle(c[i])
+for i in c:
+    shuffle(i)
 b = [choice(c) for _ in range(LENGTH)]
 
 

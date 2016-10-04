7ff41489fa0fb3cbe96093f41e5879889aa15ab7
Ezequiel Leonardo Castaño <elcg@gmx.com>
2016-10-04 08:21:56 -0300

    Fix import

--- personalscripts/Algorithms/Sorting/tests/testconf.py
+++ personalscripts/Algorithms/Sorting/tests/testconf.py
@@ -1,13 +1,13 @@
 from random import shuffle, choice
 
-def main():
-    n = 500
-    a = [i for i in range(n)]
-    LENGTH = 5
-    c = [a[:] for _ in range(LENGTH)]
-    for i in c:
-        shuffle(i)
-    b = [choice(c) for _ in range(LENGTH)]
+
+n = 500
+a = [i for i in range(n)]
+LENGTH = 5
+c = [a[:] for _ in range(LENGTH)]
+for i in c:
+    shuffle(i)
+b = [choice(c) for _ in range(LENGTH)]
 
 def per():
     return b

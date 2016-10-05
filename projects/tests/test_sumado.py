dbd66d8b3db1e81f9de2241d7560888a7e56fe9e
Ezequiel Leonardo Castaño <elcg@gmx.com>
2016-10-05 15:59:55 -0300

    Change tests directory

--- projects/tests/test_sumado.py
+++ projects/tests/test_sumado.py
@@ -4,7 +4,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
-import projects.sumado.mainfunction as main
+import sumado.mainfunction as main
 
 class TestMainFunction(unittest.TestCase):
 

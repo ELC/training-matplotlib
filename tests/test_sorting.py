75d5b6c4b1b47aea6e6c319674bb316d15c32cfd
Ezequiel Leonardo Castaño <elcg@gmx.com>
2016-10-05 20:01:08 -0300

    Convert function tests to unittests

---
+++ tests/test_sorting.py
@@ -0,0 +1,46 @@
+import context
+import algorithms.Sorting as sort
+import unittest
+import itertools
+
+
+class TestMainFunction(unittest.TestCase):
+    def __init__(self):
+        self.expected = [i for i in range(5)]
+	permutations = itertools.permutations(expected,5)
+        self.per = [list(i) for i in permutations]
+
+
+    def test_bubble_original():
+        for i in per:
+            assert sort.bubble_original.main(i) == expected
+    
+    
+    def test_false_bubble_from_i():
+        for i in per:
+            assert sort.false_bubble_from_i.main(i) == expected
+    
+    
+    def test_false_bubble_from_0():
+        for i in per:
+            assert sort.false_bubble_from_0.main(i) == expected
+    
+    
+    def test_cocktailbubble():
+        for i in per:
+            assert sort.cocktail_bubble.main(i) == expected
+    
+    
+    def test_cocktailselection():
+        for i in per:
+            assert sort.cocktail_selection.main(i) == expected
+    
+    
+    def test_insertion():
+        for i in per:
+            assert sort.insertion.main(i) == expected
+    
+    
+    def test_selection():
+        for i in per:
+            assert sort.selection.main(i) == expected

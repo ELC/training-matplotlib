ffad56574c58be7b5e9587dd12560b80425fc240
Ezequiel Leonardo Castaño <elcg@gmx.com>
2016-10-05 12:06:48 -0300

    Fix unicode string

--- docs/conf.py
+++ docs/conf.py
@@ -54,8 +54,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Training Python Projects'
-copyright = r'2016, Ezequiel Leonardo Castaño'
-author = r'Ezequiel Leonardo Castaño'
+copyright = '2016, Ezequiel C.'
+author = 'Ezequiel C.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

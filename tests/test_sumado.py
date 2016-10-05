33229829b3052f75402d4b93d7d35349b98e5312
Ezequiel Leonardo Castaño <elcg@gmx.com>
2016-10-05 15:13:48 -0300

    Import mainfunction directly

--- tests/test_sumado.py
+++ tests/test_sumado.py
@@ -4,7 +4,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../projects/sumado'))
 
-import sumado.mainfunction as main
+import mainfunction as main
 
 class TestMainFunction(unittest.TestCase):
 

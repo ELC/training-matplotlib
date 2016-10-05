1611d7c243ae692247d4eb1b280f8809b19eb496
Ezequiel Leonardo Castaño <elcg@gmx.com>
2016-10-05 16:37:19 -0300

    Fix path

--- tests/test_sumado.py
+++ tests/test_sumado.py
@@ -2,7 +2,7 @@ import unittest
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../projects/'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'projects'))
 
 import sumado.mainfunction as main
 

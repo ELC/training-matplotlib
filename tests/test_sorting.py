80b1c04725305d0b7ddc7448e8eba525d99995f2
Ezequiel Leonardo Castaño <elcg@gmx.com>
2016-10-05 20:17:33 -0300

    Fix arguments

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -7,39 +7,39 @@ expected = [i for i in range(5)]
 permutations = itertools.permutations(expected,5)
 per = [list(i) for i in permutations]
 
-class TestMainFunction(unittest.TestCase):
+class TestSortingAlgorithms(unittest.TestCase):
     """Testing for every sorting algorithm"""
 
-    def test_bubble_original():
+    def test_bubble_original(self):
         for i in per:
             assert sort.bubble_original.main(i) == expected
     
     
-    def test_false_bubble_from_i():
+    def test_false_bubble_from_i(self):
         for i in per:
             assert sort.false_bubble_from_i.main(i) == expected
     
     
-    def test_false_bubble_from_0():
+    def test_false_bubble_from_0(self):
         for i in per:
             assert sort.false_bubble_from_0.main(i) == expected
     
     
-    def test_cocktailbubble():
+    def test_cocktailbubble(self):
         for i in per:
             assert sort.cocktail_bubble.main(i) == expected
     
     
-    def test_cocktailselection():
+    def test_cocktailselection(self):
         for i in per:
             assert sort.cocktail_selection.main(i) == expected
     
     
-    def test_insertion():
+    def test_insertion(self):
         for i in per:
             assert sort.insertion.main(i) == expected
     
     
-    def test_selection():
+    def test_selection(self):
         for i in per:
             assert sort.selection.main(i) == expected
